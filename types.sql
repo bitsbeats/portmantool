c4032b5598a3bf3a2d42359464e4e709c4a55f6b
Luca Schmid <lsc@bitsbeats.com>
2021-01-18 11:51:17 +0100

    Add values 'ip', 'sctp' to `protocol` enum type

--- types.sql
+++ types.sql
@@ -1,2 +1,2 @@
-CREATE TYPE protocol	AS ENUM ('tcp', 'udp');
+CREATE TYPE protocol	AS ENUM ('ip', 'tcp', 'udp', 'sctp');
 CREATE TYPE state	AS ENUM ('open', 'closed', 'filtered', 'unfiltered', 'open|filtered', 'closed|filtered');

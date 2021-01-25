56f98b7231b386c2f977f06a0235a44be11ae1f0
Luca Schmid <lsc@bitsbeats.com>
2021-01-25 10:52:51 +0100

    Insert scan results into database (not yet optimized)

--- scanalyzer/main.go
+++ scanalyzer/main.go
@@ -44,8 +44,26 @@ func main() {
 			continue
 		}
 
-		// TODO: Insert scan results into database
 		log.Print(run)
+
+		scan := Scan{ID: time.Unix(run.Start, 0)}
+		result := db.Create(&scan)
+		if result.Error != nil {
+			log.Print(result.Error)
+			continue
+		}
+
+		// TODO: Needs optimization
+		for _, host := range run.Hosts {
+			for _, port := range host.Ports {
+				state := ActualState{Target: Target{Address: host.Address.Address, Port: port.Id, Protocol: port.Proto}, State: port.State.State, ScanID: scan.ID}
+				result = db.Create(&state)
+				if result.Error != nil {
+					log.Print(result.Error)
+					continue
+				}
+			}
+		}
 
 		err = os.Rename(reportPath, path.Join(ArchiveDir, report.Name()))
 		if err != nil {

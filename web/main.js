c3fe91b4edf8fe78f50fcb2ca434f55485655035
Luca Schmid <lsc@bitsbeats.com>
2021-11-15 18:11:57 +0100

    Add very rudimentary version of a web interface (only display diff expected/current)

---
+++ web/main.js
@@ -0,0 +1,31 @@
+class Portmantool {
+	constructor() {
+		this.apiUrl = `${window.origin}/v1`;
+	}
+
+	async get(endpoint) {
+		const response = await fetch(`${this.apiUrl}/${endpoint}`);
+
+		if (!response.ok) {
+			const message = await response.text();
+			throw message || `${response.status} ${response.statusText}`;
+		}
+
+		return response.json();
+	}
+
+	async update(endpoint, method, data) {
+		const response = await fetch(`${this.apiUrl}/${endpoint}`, {
+			method: method,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+
+		if (!response.ok) {
+			const message = await response.text();
+			throw message || `${response.status} ${response.statusText}`;
+		}
+	}
+}

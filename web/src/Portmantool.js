5587d9208784fe08e372b3ecd96cc5e342622633
Luca Schmid <lsc@bitsbeats.com>
2021-12-01 17:41:33 +0100

    Use Bulma to style expected/current state diff

--- web/src/Portmantool.js
+++ web/src/Portmantool.js
@@ -14,13 +14,24 @@ export default class Portmantool {
 		return response.json();
 	}
 
-	async update(endpoint, method, data) {
-		const response = await fetch(`${this.apiUrl}/${endpoint}`, {
-			method: method,
+	async update(data) {
+		const response = await fetch(`${this.apiUrl}/expected`, {
+			method: 'PATCH',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(data),
+		});
+
+		if (!response.ok) {
+			const message = await response.text();
+			throw message || `${response.status} ${response.statusText}`;
+		}
+	}
+
+	async prune(keep) {
+		const response = await fetch(`${this.apiUrl}/scans${keep ? `/${keep}` : ''}`, {
+			method: 'DELETE',
 		});
 
 		if (!response.ok) {

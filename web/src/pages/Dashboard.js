aae3d124d3f652be30d511fa880832329247b279
Luca Schmid <lsc@bitsbeats.com>
2021-12-08 12:20:29 +0100

    Add progress bar to dashboard

--- web/src/pages/Dashboard.js
+++ web/src/pages/Dashboard.js
@@ -3,31 +3,41 @@ import {renderTbody} from '../helpers';
 export default class Dashboard {
 	constructor(api) {
 		this.api = api;
+		this.nextUpdate = 0;
 
 		this.update();
 	}
 
 	async update() {
-		try {
-			const diff = await this.api.get('diff');
-			for (const entry of diff) {
-				entry['scan_id'] = new Date(entry['scan_id']).toLocaleString();
+		if (this.nextUpdate === 0) {
+			try {
+				const diff = await this.api.get('diff');
+				for (const entry of diff) {
+					entry['scan_id'] = new Date(entry['scan_id']).toLocaleString();
+				}
+
+				const target = document.getElementById('target');
+				renderTbody(target, diff, [
+					'address',
+					'port',
+					'protocol',
+					'expected_state',
+					'actual_state',
+					'scan_id',
+					'comment',
+				]);
+			} catch (error) {
+				console.log(error);
 			}
 
-			const target = document.getElementById('target');
-			renderTbody(target, diff, [
-				'address',
-				'port',
-				'protocol',
-				'expected_state',
-				'actual_state',
-				'scan_id',
-				'comment',
-			]);
-		} catch (error) {
-			console.log(error);
+			this.nextUpdate = 5;
 		}
 
-		setTimeout(() => this.update(), 5000);
+		--this.nextUpdate;
+
+		const progress = document.getElementById('nextUpdate');
+		progress.value = 4 - this.nextUpdate;
+
+		setTimeout(() => this.update(), 1000);
 	}
 }
